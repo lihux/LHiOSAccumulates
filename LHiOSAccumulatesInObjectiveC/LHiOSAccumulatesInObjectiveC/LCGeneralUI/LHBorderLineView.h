3f6726bf8be41e6b1a112a1cd5b7ce9adef7b1b1
李辉 <lihui.zl@163.com>
2016-01-13 19:53:34 +0800

    <OC>add custom border line view

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCGeneralUI/LHBorderLineView.h
@@ -0,0 +1,13 @@
+//
+//  LHBorderLineView.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 16/1/13.
+//  Copyright © 2016年 Lihux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHBorderLineView : UIView
+
+@end
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCGeneralUI/LHBorderLineView.m
@@ -0,0 +1,24 @@
+//
+//  LHBorderLineView.m
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 16/1/13.
+//  Copyright © 2016年 Lihux. All rights reserved.
+//
+
+#import "LHBorderLineView.h"
+
+@implementation LHBorderLineView
+
+- (void)awakeFromNib
+{
+    for (NSLayoutConstraint *constraint in self.constraints) {
+        NSLog(@"%@", constraint);
+        if (constraint.firstItem == self && (constraint.firstAttribute == NSLayoutAttributeWidth || constraint.firstAttribute == NSLayoutAttributeHeight)) {
+            constraint.constant = 1 / [UIScreen mainScreen].scale;
+            [self layoutIfNeeded];
+        }
+    }
+}
+
+@end
