4fb99e0e9f72e74f2b1028a44b47cffe48dca965
李辉 <lihui.zl@163.com>
2016-01-14 22:21:18 +0800

    <OC>-add button flying and zooming animating for app launch and button tap actions

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/AppDelegate.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [NSThread sleepForTimeInterval:1.5];
     // Override point for customization after application launch.
     return YES;
 }
--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCHome/LCHomeViewController.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCHome/LCHomeViewController.m
@@ -10,6 +10,9 @@
 
 #import "LCLightBorderButton.h"
 
+static const CGFloat kDefaultFlyingAnimationDuration = 1.0;
+static const CGFloat kDefaultZoomingAnimationDuration = 0.5;
+
 @interface LCHomeViewController ()
 
 @property (strong, nonatomic) IBOutletCollection(LCLightBorderButton) NSArray *homeButtons;
@@ -25,8 +28,14 @@
 @property (strong, nonatomic) IBOutletCollection(NSLayoutConstraint) NSArray *flyView3SizeConstraints;
 @property (strong, nonatomic) IBOutletCollection(NSLayoutConstraint) NSArray *flyView4SizeConstraints;
 
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *flyingViews;
+
+
 @property (nonatomic, strong) NSArray *flyConstraintArray;
 @property (nonatomic, assign) NSInteger flyingViewIndex;
+@property (nonatomic, assign) BOOL firstLaunch;
+@property (nonatomic, strong) NSArray *homeButtonTitles;
+@property (nonatomic, assign) BOOL isFlying;
 
 @end
 
@@ -35,9 +44,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self adjustLayoutConstraints];
-    self.flyConstraintArray = @[self.flyView1SizeConstraints, self.flyView2SizeConstraints, self.flyView3SizeConstraints, self.flyView4SizeConstraints];
+    [self prepareDatas];
     self.flyingViewIndex = -1;
-    self.maskLabel.alpha = 0;
+    self.homeButtonTitles = @[@"好玩", @"好看", @"你猜", @"设置"];
+}
+
+- (void)prepareDatas
+{
+    self.flyConstraintArray = @[self.flyView1SizeConstraints, self.flyView2SizeConstraints, self.flyView3SizeConstraints, self.flyView4SizeConstraints];
+    self.flyingViews = [self.flyingViews sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        NSComparisonResult result = NSOrderedAscending;
+        if ([(UIView *)obj1 tag] > [(UIView *)obj1 tag]) {
+            result = NSOrderedDescending;
+        }
+        return result;
+    }];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (!self.firstLaunch) {
+        self.firstLaunch = YES;
+        [self homeButtonAnimateWithButtonIndex:0 isFlyingAway:YES completion:^{
+            self.maskLabel.text = self.homeButtonTitles[0];
+            [self centerCircleAnimateWithIsZooming:YES completion:^{
+                self.flyingViewIndex = 0;
+            }];
+        }];
+    }
 }
 
 #pragma mark - AutoLayout Concerns
@@ -56,54 +91,63 @@
     [self.view layoutIfNeeded];
 }
 
-- (void)animatingButton:(BOOL)isZoom
+- (void)centerCircleAnimateWithIsZooming:(BOOL)isZooming completion:(void(^)())completion
 {
-    CGFloat width = isZoom ? [UIScreen mainScreen].bounds.size.width * 2 : 50;
-    CGFloat scale = width / 50;
-    CGFloat alpha = isZoom ? 0 : 1;
-    [UIView animateWithDuration:0.8 animations:^{
+    self.maskLabel.hidden = NO;
+    self.maskView.hidden = NO;
+    if (!isZooming) {
+        self.maskLabel.text = self.homeButtonTitles[self.flyingViewIndex];
+    }
+    CGFloat scale = isZooming ? self.view.bounds.size.width * 2 / 50 : 1;
+    CGFloat labelAlpha = isZooming ? 0 : 1;
+    [UIView animateWithDuration:kDefaultZoomingAnimationDuration animations:^{
         self.maskView.transform = CGAffineTransformMakeScale(scale, scale);
-        self.maskLabel.alpha = alpha;
+        self.maskLabel.alpha = labelAlpha;
+    } completion:^(BOOL finished) {
+        if (finished && completion) {
+            self.maskLabel.hidden = YES;
+            self.maskView.hidden = !isZooming;
+            completion();
+        }
+    }];
+}
+
+- (void)homeButtonAnimateWithButtonIndex:(NSInteger)index isFlyingAway:(BOOL)isFlyingAway completion:(void(^)())completion
+{
+    [(UIView *)self.flyingViews[index] setHidden:NO];
+    BOOL stopHomeButtonFlyAwayConstraintActive = !isFlyingAway;
+    NSArray *constraints = self.flyConstraintArray[index];
+    for (NSLayoutConstraint *constraint in constraints) {
+        constraint.active = stopHomeButtonFlyAwayConstraintActive;
+    }
+    [UIView animateWithDuration:kDefaultFlyingAnimationDuration animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        if (finished && completion) {
+            [(UIView *)self.flyingViews[index] setHidden:YES];
+            completion();
+        }
     }];
 }
 
 - (IBAction)didTapOnHomeButton:(LCLightBorderButton *)sender {
     NSInteger index = sender.tag;
-    if (index == self.flyingViewIndex) {
+    if (index == self.flyingViewIndex || self.isFlying) {
         return;
     }
-    CGFloat kAnimationDuration = 0.5;
-    NSArray *constraintsToBeDeactive = self.flyConstraintArray[index - 1];
-    NSArray *constraintsToBeActive = nil;
-    if (self.flyingViewIndex > 0) {
-        constraintsToBeActive = self.flyConstraintArray[self.flyingViewIndex - 1];
-        [constraintsToBeActive enumerateObjectsUsingBlock:^(NSLayoutConstraint *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            obj.active = YES;
+    
+    NSInteger currentIndex = self.flyingViewIndex;
+    self.isFlying = YES;
+    [self centerCircleAnimateWithIsZooming:NO completion:^{
+        [self homeButtonAnimateWithButtonIndex:currentIndex isFlyingAway:NO completion:^{
         }];
-        [UIView animateWithDuration:kAnimationDuration animations:^{
-            [self.view layoutIfNeeded];
-        } completion:^(BOOL finished) {
-            [constraintsToBeDeactive enumerateObjectsUsingBlock:^(NSLayoutConstraint *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                obj.active = NO;
-            }];
-            [UIView animateWithDuration:kAnimationDuration animations:^{
-                [self.view layoutIfNeeded];
-            } completion:^(BOOL finished) {
-                self.flyingViewIndex = index;
-            }];
-        }];
-    } else {
-        [constraintsToBeDeactive enumerateObjectsUsingBlock:^(NSLayoutConstraint *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            obj.active = NO;
-        }];
-        [UIView animateWithDuration:kAnimationDuration animations:^{
-            self.maskLabel.alpha = 1;
-            [self.view layoutIfNeeded];
-        } completion:^(BOOL finished) {
+    }];
+    [self homeButtonAnimateWithButtonIndex:index isFlyingAway:YES completion:^{
+        [self centerCircleAnimateWithIsZooming:YES completion:^{
             self.flyingViewIndex = index;
-            [self animatingButton:YES];
+            self.isFlying = NO;
         }];
-    }
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
