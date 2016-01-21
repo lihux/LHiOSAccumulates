8df6193488a9139391a6aa90948daae2ee8da5c9
李辉 <lihui.zl@163.com>
2016-01-21 23:02:22 +0800

    <OC> add LCDemo01: UI skills: using of storyboard and xib etc.

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCDemo/LCDemo01/LCDemo01UIViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCDemo01UIViewController.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 16/1/21.
+//  Copyright © 2016年 Lihux. All rights reserved.
+//
+
+#import "LCTableViewController.h"
+
+@interface LCDemo01UIViewController : LCTableViewController
+
+@end
