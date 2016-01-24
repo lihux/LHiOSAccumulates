7e625f8f05339c137f444c1df7fe811c3e9d48f5
李辉 <lihui.zl@163.com>
2016-01-24 12:32:40 +0800

    <swift> add color extension for conveniences

---
+++ LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/LSGenreal/LSHelper/UIColor+Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Utilities.swift
+//  LHiOSAccumulatesInSwift
+//
+//  Created by lihui on 16/1/24.
+//  Copyright © 2016年 Lihux. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+extension UIColor
+{
+    
+}
