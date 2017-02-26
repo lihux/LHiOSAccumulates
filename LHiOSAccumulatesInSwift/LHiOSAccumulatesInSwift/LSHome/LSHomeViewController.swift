45184d2327a302688ec0fd59ea76b58f88dbc451
lihux <lihui.zl@163.com>
2017-02-26 19:41:23 +0800

    <Swift>调试完成swift首页动画及其他逻辑
    现在首页终于可以正常跑起来了，虽然数据还没有

--- LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/LSHome/LSHomeViewController.swift
+++ LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/LSHome/LSHomeViewController.swift
@@ -36,11 +36,80 @@ class LSHomeViewController: LSBasicViewController {
     }
 
     var flyConstraintArray:[[NSLayoutConstraint]]?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.adjustLayoutConstraints()
         self.prepareDatas()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.updateControllerControllerContrainerViewWithZoomingState(isZooming: false)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if self.firstLaunch {
+            self.firstLaunch = false
+            self.updateFlyingIndex(index: 0)
+            (self.homeButtons[0] as UIButton).isSelected = true
+            self.homeButtonAnimateWithButtonIndex(index: 0, isFlyingAway: true, customCompletion: { () in
+                self.maskLabel.text = self.homeButtonTitles[0]
+                self.centerCircleAnimateWithIsZooming(isZooming: true, customCompletion: nil)
+            })
+        }
+    }
+
+    func homeButtonAnimateWithButtonIndex(index:Int, isFlyingAway:Bool, customCompletion:((Void) -> Void)?) {
+        let flyingView = self.flyingViews[index] as UIView
+        flyingView.isHidden = false
+        let stopHomeButtonFlyAwayConstraintActive = !isFlyingAway
+        let constraints = (self.flyConstraintArray?[index])! as [NSLayoutConstraint]
+        for constraint in constraints {
+            constraint.isActive = stopHomeButtonFlyAwayConstraintActive
+        }
+        UIView.animate(withDuration: 0.6, animations: { 
+            flyingView.alpha = isFlyingAway ? 1.0 : 0.4
+            self.view.layoutIfNeeded()
+        }) { (finished) in
+            if finished {
+                flyingView.isHidden = true
+                if let temp = customCompletion {
+                    temp()
+                }
+            }
+        }
+    }
+    
+    func centerCircleAnimateWithIsZooming(isZooming:Bool, customCompletion: ((Void) -> Void)?) {
+        self.maskLabel.isHidden = false
+        self.maskView.isHidden = false
+        self.maskLabel.text = self.homeButtonTitles[self.flyingViewIndex]
+        let scale = isZooming ? self.view.bounds.size.width * 2 / 50 : 1
+        let labelAlpha:CGFloat = isZooming ? 0 : 1
+        UIView.animate(withDuration: 0.4, animations: { 
+            self.maskView.transform = CGAffineTransform.identity.scaledBy(x: scale, y: scale)
+            self.maskLabel.alpha = labelAlpha
+            self.updateControllerControllerContrainerViewWithZoomingState(isZooming: isZooming)
+        }) { (finished) in
+            if finished {
+                self.maskLabel.isHidden = true
+                self.maskView.isHidden = !isZooming
+                if let temp = customCompletion {
+                    temp()
+                }
+            }
+        }
+    }
+    
+    func updateFlyingIndex(index: Int) {
+        self.flyingViewIndex = index
+        if flyingViewIndex >= 0 {
+            for view in self.subViewControllerContainerViews {
+                view.isHidden = !(flyingViewIndex == view.tag)
+            }
+        }
     }
 
     func adjustLayoutConstraints() {
@@ -62,9 +131,34 @@ class LSHomeViewController: LSBasicViewController {
             view1.tag < view2.tag
         })
     }
-    @IBAction func didTapOnHomeButton(_ sender: LSLightBorderButton) {
-        
+
+    func updateControllerControllerContrainerViewWithZoomingState(isZooming: Bool) {
+        let controllerContainerViewScale = isZooming ? 1 : self.self.controllerContainerViewZoomingMin
+        let controllerContainerViewAlpha = isZooming ? 1 : 0
+        self.controllerContrainerView.transform = CGAffineTransform.identity.scaledBy(x: controllerContainerViewScale, y: controllerContainerViewScale)
+        self.controllerContrainerView.alpha = CGFloat(controllerContainerViewAlpha)
     }
+
+    @IBAction func didTapOnHomeButton(_ sender: LSLightBorderButton) {
+        let index = sender.tag
+        if (index == self.flyingViewIndex || self.isFlying) {
+            return
+        }
+        let currentIndex = self.flyingViewIndex
+        sender.isSelected = true
+        self.homeButtons[currentIndex].isSelected = false
+        self.isFlying = true
+        self.centerCircleAnimateWithIsZooming(isZooming: false) { () in
+            self.homeButtonAnimateWithButtonIndex(index: currentIndex, isFlyingAway: false, customCompletion: nil)
+        }
+        self.homeButtonAnimateWithButtonIndex(index: index, isFlyingAway: true) { () in
+            self.flyingViewIndex = index;
+            self.centerCircleAnimateWithIsZooming(isZooming: true, customCompletion: { () in
+                self.isFlying = false
+            })
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
