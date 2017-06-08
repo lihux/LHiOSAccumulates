86f36bdd980cfc1c974c28eb47b5caff08903ceb
lihux <lihui.zl@163.com>
2017-06-08 10:23:02 +0800

    <OC>添加添加子view自动布局辅助分类

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/General/LHHelpers/UIView+LHAutoLayout.h
@@ -0,0 +1,46 @@
+//
+//  UIView+LHAutoLayout.h
+//  jiaxiaozhijia-ios
+//
+//  Created by 李辉 on 2017/5/5.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+typedef struct {
+    CGFloat top;
+    CGFloat left;
+    CGFloat bottom;
+    CGFloat right;
+    CGFloat width;
+    CGFloat height;
+} LHLayoutInfo;
+
+UIKIT_STATIC_INLINE  LHLayoutInfo LHLayoutInfoMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right, CGFloat width, CGFloat height) {
+    LHLayoutInfo info = {top, left, bottom, right, width, height};
+    return info;
+}
+
+static const CGFloat LHLayoutNone = CGFLOAT_MAX;
+
+@interface UIView (LHAutoLayout)
+
+/**
+ add a subview using layout constraints: @"H:|-0-[_tableView]-0-|" @"V:|-0-[_tableView]-0-|"
+ 
+ @param view subview that will be added using default constraints:@"H:|-0-[_tableView]-0-|" @"V:|-0-[_tableView]-0-|"
+ */
+- (void)addSubviewUsingDeaultLayoutConstraints:(UIView *)view;
+
+
+/**
+ 高效的添加一个view作为subView，同时设置其相对于父view的布局信息,布局基于VFL语言：http://commandshift.co.uk/blog/2013/01/31/visual-format-language-for-autolayout/
+ 目前仅支持(top, left, bottom, right, width, height)共6个维度的布局添加布局，这基本上覆盖了80%的业务需求，如需更复杂的布局，请直接添加
+ 
+ @param view 需要添加的subView
+ @param info 布局信息，根据需要可以提供上、左、下、右、宽、高六种布局信息，如果不需要添加的约束，info相应的字段可以用`LHLayoutNone`设置
+ */
+- (void)addSubview:(UIView *)view withLayoutInfo:(LHLayoutInfo)info;
+
+@end
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/General/LHHelpers/UIView+LHAutoLayout.m
@@ -0,0 +1,65 @@
+//
+//  UIView+LHAutoLayout.m
+//  jiaxiaozhijia-ios
+//
+//  Created by 李辉 on 2017/5/5.
+//
+//
+
+#import "UIView+LHAutoLayout.h"
+
+@implementation UIView (LHAutoLayout)
+
+- (void)addSubviewUsingDeaultLayoutConstraints:(UIView *)view {
+    if (!view) {
+        return;
+    }
+    [self addSubview:view];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[view]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[view]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)]];
+}
+
+- (void)addSubview:(UIView *)view withLayoutInfo:(LHLayoutInfo)info {
+    [self addSubview:view];
+    [self layoutView:view constant:info.top name:NSLayoutAttributeTop];
+    [self layoutView:view constant:info.left name:NSLayoutAttributeLeft];
+    [self layoutView:view constant:info.bottom name:NSLayoutAttributeBottom];
+    [self layoutView:view constant:info.right name:NSLayoutAttributeRight];
+    [self layoutView:view constant:info.width name:NSLayoutAttributeWidth];
+    [self layoutView:view constant:info.height name:NSLayoutAttributeHeight];
+}
+
+- (void)layoutView:(UIView *)view constant:(CGFloat)constant name:(NSLayoutAttribute)attribute {
+    if (constant != LHLayoutNone) {
+        NSString *vfl = nil;
+        NSDictionary *metrics = @{@"constant": @(constant)};
+        switch (attribute) {
+            case NSLayoutAttributeTop:
+                vfl = @"V:|-constant-[view]";
+                break;
+            case NSLayoutAttributeLeft:
+                vfl = @"H:|-constant-[view]";
+                break;
+            case NSLayoutAttributeBottom:
+                vfl = @"V:[view]-constant-|";
+                break;
+            case NSLayoutAttributeRight:
+                vfl = @"H:[view]-constant-|";
+                break;
+            case NSLayoutAttributeWidth:
+                vfl = @"H:[view(constant)]";
+                break;
+            case NSLayoutAttributeHeight:
+                vfl = @"V:[view(constant)]";
+                break;
+                
+            default:
+                break;
+        }
+        if (vfl) {
+            [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:vfl options:0 metrics:metrics views:NSDictionaryOfVariableBindings(view)]];
+        }
+    }
+}
+
+@end
