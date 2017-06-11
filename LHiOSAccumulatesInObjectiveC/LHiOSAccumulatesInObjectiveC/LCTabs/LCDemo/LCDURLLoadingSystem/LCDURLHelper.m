aebe7f0ee36789fa6c5a5b49f83d5a68adc83786
lihux <lihui.zl@163.com>
2017-06-11 19:56:01 +0800

    <OC>host 修改后面的path:/accumulates/v1

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDURLLoadingSystem/LCDURLHelper.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDURLLoadingSystem/LCDURLHelper.m
@@ -8,7 +8,7 @@
 
 #import "LCDURLHelper.h"
 
-NSString * const kLCDURLHost = @"http://13.58.220.233:1113/v1/accumulates";
+NSString * const kLCDURLHost = @"http://13.58.220.233:1113/accumulates/v1";
 
 @implementation LCDURLHelper
 
