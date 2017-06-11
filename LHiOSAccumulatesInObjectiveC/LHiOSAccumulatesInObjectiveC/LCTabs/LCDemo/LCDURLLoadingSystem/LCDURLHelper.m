2b15afbbab325fa6f5b8a3525b2234f1147482c9
lihux <lihui.zl@163.com>
2017-06-11 19:51:19 +0800

    修改host

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDURLLoadingSystem/LCDURLHelper.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDURLLoadingSystem/LCDURLHelper.m
@@ -8,7 +8,7 @@
 
 #import "LCDURLHelper.h"
 
-NSString * const kLCDURLHost = @"http://13.58.220.233:8080";
+NSString * const kLCDURLHost = @"http://13.58.220.233:1113/v1/accumulates/";
 
 @implementation LCDURLHelper
 
