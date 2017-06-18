f0498f3be16924758cef3fbac8db4a1fab989304
lihux <lihui.zl@163.com>
2017-06-18 21:43:02 +0800

    <OC>block的实现探究（一）

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCDBBlockExploreViewController.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCDBBlockExploreViewController.m
@@ -8,7 +8,11 @@
 
 #import "LCDBBlockExploreViewController.h"
 
+typedef void(^LCDBCompletionBlock)(BOOL);
+
 @interface LCDBBlockExploreViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 
 @end
 
@@ -16,7 +20,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSInteger aa = 1;
+    NSInteger *bb = &aa;
+    NSString *cc = @"hello";
+    __block NSString *dd = [NSString stringWithFormat:@"%@", cc];
+    NSArray *ee = @[@1, @2];
+    NSMutableArray *ff = [NSMutableArray arrayWithArray:ee];
+    [self log:[NSString stringWithFormat:@"地址信息是：aa: %lld, bb: %lld, cc: %lld, dd: %lld, ee: %lld, ff: %lld", &aa, &bb, &cc, &dd, &ee, &ff]];
+    [self testBlock:^(BOOL finished) {
+        NSInteger aaa = aa;
+        NSInteger *bbb = bb;
+        NSInteger bbbb = *bb;
+        NSArray *eee = ee;
+        
+        NSString *ddd = dd;
+        dd = @"周杰伦";
+        NSLog(@"ddd:%@,dd:%@,&ddd:%lld,&dd:%lld", ddd, dd , &ddd, &dd);
+        NSNumber *fff = [ff objectAtIndex:1];
+        if (finished) {
+            [self log:@"block被调用"];
+        }
+    }];
+}
+- (UIView *)logAnchorView {
+    return self.contentView;
+}
+
+- (void)testBlock:(LCDBCompletionBlock)block {
+    if (block) {
+        block(YES);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
