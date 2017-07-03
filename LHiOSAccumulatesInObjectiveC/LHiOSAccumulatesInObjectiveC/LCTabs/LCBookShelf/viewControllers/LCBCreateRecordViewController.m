8cd2210e30126ee4d42951ae8ba9d0c3471a5f6a
lihux <lihui.zl@163.com>
2017-07-03 23:05:02 +0800

    <OC>计划记录页完成输入逻辑的开发“

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCBookShelf/viewControllers/LCBCreateRecordViewController.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCBookShelf/viewControllers/LCBCreateRecordViewController.m
@@ -9,12 +9,15 @@
 #import "LCBCreateRecordViewController.h"
 
 #import "LCBookShelf+CoreDataModel.h"
+#import "UIView+LHAutoLayout.h"
 
 @interface LCBCreateRecordViewController ()
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *progressViewWidthConstraint;
 @property (weak, nonatomic) IBOutlet UILabel *progressInfoLabel;
 @property (nonatomic, strong) LCBookReadingPlan *plan;
+@property (nonatomic, strong) UIView *inputMaskView;
+@property (weak, nonatomic) IBOutlet UITextField *finishPageTextField;
 
 @end
 
@@ -33,7 +36,29 @@
 }
 
 - (void)customUI {
-    
+    [self.view addSubviewUsingDefaultLayoutConstraints:self.inputMaskView];
+}
+
+- (void)didTapOnInputMaskView {
+    [self.finishPageTextField resignFirstResponder];
+}
+
+- (IBAction)didTapOnPageTextField:(id)sender {
+    self.inputMaskView.hidden = NO;
+}
+
+#pragma mark - lazy load
+- (UIView *)inputMaskView {
+    if (_inputMaskView) {
+        return _inputMaskView;
+    }
+    _inputMaskView = [[UIView alloc] init];
+    _inputMaskView.backgroundColor = [UIColor clearColor];
+    _inputMaskView.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapOnInputMaskView)];
+    [_inputMaskView addGestureRecognizer:tap];
+    _inputMaskView.hidden = YES;
+    return _inputMaskView;
 }
 
 @end
