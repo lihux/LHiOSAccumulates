ca235cb7e38e0da8278b598c4618e34999a41467
lihux <lihui.zl@163.com>
2017-07-05 07:44:41 +0800

    <OC>exchange代码生成汇编命令探查

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/126_exchangeO2.s
@@ -0,0 +1,23 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_exchange
+	.p2align	4, 0x90
+_exchange:                              ## @exchange
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	movq	(%rdi), %rax
+	movq	%rsi, (%rdi)
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+
+.subsections_via_symbols
