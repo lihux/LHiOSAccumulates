f3c6d944395d9020da1a2c1d8528db5a89398741
lihux <lihui.zl@163.com>
2017-07-10 20:09:40 +0800

    <OC>混合两种颜色，找到办法啦。。。

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/General/LHHelpers/UIColor+helper.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/General/LHHelpers/UIColor+helper.m
@@ -30,4 +30,16 @@
     return [UIColor colorWithRed:((value & 0xFF0000) >> 16) / 255.0 green:((value & 0x00FF00) >> 8) / 255.0 blue:(value & 0x0000FF) / 255.0 alpha:alpha];
 }
 
+//UIColor* blend( UIColor* c1, UIColor* c2, float alpha )
+//{
+//    alpha = MIN( 1.f, MAX( 0.f, alpha ) );
+//    float beta = 1.f - alpha;
+//    CGFloat r1, g1, b1, a1, r2, g2, b2, a2;
+//    [c1 getRed:&r1 green:&g1 blue:&b1 alpha:&a1];
+//    [c2 getRed:&r2 green:&g2 blue:&b2 alpha:&a2];
+//    CGFloat r = r1 * beta + r2 * alpha;
+//    CGFloat g = g1 * beta + g2 * alpha;
+//    CGFloat b = b1 * beta + b2 * alpha;
+//    return [UIColor colorWithRed:r green:g blue:b alpha:1.f];
+//}
 @end
