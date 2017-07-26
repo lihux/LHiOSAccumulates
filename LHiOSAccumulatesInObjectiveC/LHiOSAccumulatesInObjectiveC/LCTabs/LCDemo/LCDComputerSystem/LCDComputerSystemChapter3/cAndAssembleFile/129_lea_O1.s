436ee758379fd0f95187b79bf8ba5a58aad12792
lihux <lihui.zl@163.com>
2017-07-26 19:53:43 +0800

    <OC>llvm-gcc -O1 -S -o 129_lea.s 129_lea.c 发现编译优化代码的时候果然使用了lea，于是心情很愉悦

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/129_lea_O1.s
@@ -0,0 +1,24 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_scale
+	.p2align	4, 0x90
+_scale:                                 ## @scale
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	leaq	(%rdi,%rsi,4), %rax
+	leaq	(%rdx,%rdx,2), %rcx
+	leaq	(%rax,%rcx,4), %rax
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+
+.subsections_via_symbols
