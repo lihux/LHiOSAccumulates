388a1ae7627428aeebb86c7da295652b0e785658
lihux <lihui.zl@163.com>
2017-08-04 06:38:00 +0800

    <OC>llvm-gcc -O0/O1 -S -o 217_store_prod_O0/1.s 217_store_prod.c GCC实现128位整形的乘法细节

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/217_store_prod.c
@@ -0,0 +1,4 @@
+typedef __int128 int128_t;
+void store_prod(int128_t *dest, int x, int y) {
+    *dest = x * (int128_t)y;
+}
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/217_store_prod_O0.s
@@ -0,0 +1,30 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_store_prod
+	.p2align	4, 0x90
+_store_prod:                            ## @store_prod
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movl	%edx, -16(%rbp)
+	movslq	-12(%rbp), %rax
+	movslq	-16(%rbp), %rdi
+	imulq	%rdi
+	movq	-8(%rbp), %rdi
+	movq	%rdx, 8(%rdi)
+	movq	%rax, (%rdi)
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+
+.subsections_via_symbols
