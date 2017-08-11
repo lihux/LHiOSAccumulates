80f2d96ebe61a9a87067bd704a8c07e11cdc29b9
lihux <lihui.zl@163.com>
2017-08-12 07:09:12 +0800

    <OC>找到原因了：只有当优化级别到达-O2/O3的时候，编译器才会对内联方法进行就地替换

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/344_inline_call_f_O2.s
@@ -0,0 +1,48 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_func1
+	.p2align	4, 0x90
+_func1:                                 ## @func1
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	movq	_counter(%rip), %rax
+	leaq	4(%rax), %rcx
+	movq	%rcx, _counter(%rip)
+	leaq	6(,%rax,4), %rax
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.globl	_func2
+	.p2align	4, 0x90
+_func2:                                 ## @func2
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp3:
+	.cfi_def_cfa_offset 16
+Ltmp4:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp5:
+	.cfi_def_cfa_register %rbp
+	movq	_counter(%rip), %rax
+	leaq	1(%rax), %rcx
+	movq	%rcx, _counter(%rip)
+	shlq	$2, %rax
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.globl	_counter                ## @counter
+.zerofill __DATA,__common,_counter,8,3
+
+.subsections_via_symbols
