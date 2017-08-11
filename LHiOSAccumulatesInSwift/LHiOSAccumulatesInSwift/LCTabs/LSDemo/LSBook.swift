dc2cf09d4701e3c182798bdd2680173a7f154394
lihux <lihui.zl@163.com>
2017-08-11 18:44:45 +0800

    <Swift>添加LSBook struct,还没来得及填充codable

---
+++ LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/LCTabs/LSDemo/LSBook.swift
@@ -0,0 +1,13 @@
+//
+//  LSBook.swift
+//  LHiOSAccumulatesInSwift
+//
+//  Created by 李辉 on 2017/8/11.
+//  Copyright © 2017年 Lihux. All rights reserved.
+//
+
+import Foundation
+
+struct LSBook: Codable {
+    //
+}
