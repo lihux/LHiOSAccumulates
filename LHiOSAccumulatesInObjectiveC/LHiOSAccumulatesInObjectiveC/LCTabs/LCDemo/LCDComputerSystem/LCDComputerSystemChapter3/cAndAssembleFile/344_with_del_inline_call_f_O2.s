5fa0a1206b61b638f51d0116648df3f5bc9b32b6
lihux <lihui.zl@163.com>
2017-08-12 07:15:06 +0800

    <OC>发现声明的地方去掉了inline 汇编代码有细微差别

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/344_with_del_inline_call_f_O2.s
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter3/cAndAssembleFile/344_with_del_inline_call_f_O2.s
@@ -42,26 +42,6 @@ Ltmp5:
 	retq
 	.cfi_endproc
 
-	.globl	_f
-	.p2align	4, 0x90
-_f:                                     ## @f
-	.cfi_startproc
-## BB#0:
-	pushq	%rbp
-Ltmp6:
-	.cfi_def_cfa_offset 16
-Ltmp7:
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-Ltmp8:
-	.cfi_def_cfa_register %rbp
-	movq	_counter(%rip), %rax
-	leaq	1(%rax), %rcx
-	movq	%rcx, _counter(%rip)
-	popq	%rbp
-	retq
-	.cfi_endproc
-
 	.globl	_counter                ## @counter
 .zerofill __DATA,__common,_counter,8,3
 
