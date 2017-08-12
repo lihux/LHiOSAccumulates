891b89c9cd813a58d8f4f1744577a45e722673bd
lihux <lihui.zl@163.com>
2017-08-12 22:21:27 +0800

    <Swift>添加条形码扫描页面，完成扫描功能，尚未利用

---
+++ LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/LCTabs/LSDemo/LSBookScanViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LSBookScanViewController.swift
+//  LHiOSAccumulatesInSwift
+//
+//  Created by lihui on 2017/8/12.
+//  Copyright © 2017年 Lihux. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class LSBookScanViewController: LSViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var session: AVCaptureSession?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.startScan()
+    }
+    
+    func startScan() -> Void {
+        let device = AVCaptureDevice.default(for: .video)
+        do {
+            let input = try AVCaptureDeviceInput.init(device: device!)
+            let output = AVCaptureMetadataOutput()
+            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            session = AVCaptureSession()
+            session!.sessionPreset = .high
+            session!.addInput(input)
+            session!.addOutput(output)
+            output.metadataObjectTypes = [.qr, .ean13, .ean8, .code128]
+            let layer = AVCaptureVideoPreviewLayer(session: session!)
+            layer.videoGravity = .resizeAspectFill
+            layer.frame = CGRect(x: 10, y: 100, width: 300, height: 200)
+            self.view.layer.addSublayer(layer)
+            session!.startRunning()
+        } catch let error as NSError {
+            print("\(error.localizedDescription):\n\(error.userInfo)")
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects.count > 0 {
+            session!.stopRunning()
+            let metaDataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            print("扫描成功，条形码信息为：\(metaDataObject.stringValue!)")
+        }
+    }
+    
+}
