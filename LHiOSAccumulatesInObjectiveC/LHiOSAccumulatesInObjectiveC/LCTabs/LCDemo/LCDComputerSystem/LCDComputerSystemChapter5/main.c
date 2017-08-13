a40e4e049d36e4cd01d2d5b703ec209d797462c3
lihux <lihui.zl@163.com>
2017-08-14 06:28:27 +0800

    <OC>比较了一下clock()和time()决定还是使用clock（ms)来统计程序运行时间（1s == 1000,000ms）

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter5/main.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter5/main.c
@@ -3,12 +3,16 @@
 
 int main () {
     double start, end, cost;
+    time_t t_start, t_end;
     start = clock();
-    int j = 0;
-    for (int i = 0; i < 1000; i ++) {
-        j ++;
+    t_start = time(NULL);
+    float j = 432432;
+    for (int i = 0; i < 100000000; i ++) {
+        j /= 1.109;
     }
     end = clock();
+    t_end = time(NULL);
     cost = end - start;
-    printf("睡你一秒占用的CPU时间是：%f", cost);
+    printf("占用的CPU时间是：%f, j = %f", cost, j);
+    printf("占用的时间是：%f", difftime(t_end, t_start));
 }
