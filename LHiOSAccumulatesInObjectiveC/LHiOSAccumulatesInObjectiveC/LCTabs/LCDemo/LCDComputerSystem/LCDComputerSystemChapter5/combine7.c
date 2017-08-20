9a8ab126769298c970b4f53c6c84008b479d8d25
lihux <lihui.zl@163.com>
2017-08-21 07:40:00 +0800

    <OC>llvm-gcc -O0/1/2/3 -S -o combine7_O0/1/2/3.s combine7.c 在6的基础上，通过添加括号改变求值顺序进一步优化数据依赖的情况

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter5/combine7.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter5/combine7.c
@@ -10,19 +10,17 @@
 void combine1(vec_ptr v, data_t *dest) {
     long i;
     
-    data_t acc0 = IDENT;
-    data_t acc1 = IDENT;
+    data_t acc = IDENT;
     long length = vec_length(v);
     long limit = length - 1;
     data_t *data = get_vec_start(v);
     
     for (i = 0; i < limit; i+=2) {
-        acc0 = acc0 OP (data[i] OP data[i+1]);
-        acc1 = acc1 OP (data[i] OP data[i+1]);
+        acc = (acc OP data[i]) OP data[i+1];
     }
     for (; i < length; i ++) {
-        acc0 = acc0 OP data[i];
+        acc = acc OP data[i];
     }
-    *dest = acc0 OP acc1;
+    *dest = acc;
 }
 
