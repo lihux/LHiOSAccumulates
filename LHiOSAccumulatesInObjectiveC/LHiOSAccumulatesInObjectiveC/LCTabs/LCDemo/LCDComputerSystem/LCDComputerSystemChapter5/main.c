c4c6bc1354eb31b8c026737cbc329d99f13efc9f
lihux <lihui.zl@163.com>
2017-08-22 05:57:36 +0800

    <OC>添加对CSAPP_CH5优化代码性能的总结

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter5/main.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter5/main.c
@@ -1,12 +1,27 @@
 #include <time.h>
 #include <stdio.h>
 
+
+
 #include "combine1.h"
 
 static const long kLENGTH = 100;
 
 void calculate();
 
+/**
+ 小结：本代码来自《CSAPP》第五章：优化程序性能，通过以下若干步骤，逐步优化：
+ 1.消除循环的低效率：通过代码移动（code motion）将循环内部所做的事情尽可能的少；
+ 2.发现隐藏的渐进低效率的地方（asymptotic inefficiency）改进之；
+ 3.减少过程调用；
+ 4.消除不必要的内存引用；
+ 5.理解现有CPU的基础上，优化代码，使之在CPU流水中更顺畅；
+ 6.循环展开；
+ 7.多个累积变量（充分利用现代CPU多个功能单元的特性，缩小流水间隔）；
+ 8.重新结合变换；
+ 
+ @return 0
+ */
 int main () {
     double start, end, cost;
     time_t t_start, t_end;
@@ -15,6 +30,7 @@ int main () {
     end = clock();
     cost = end - start;
     printf("\n程序执行时间为：%lf\n\n", cost);
+    return 0;
 }
 
 void calculate() {
