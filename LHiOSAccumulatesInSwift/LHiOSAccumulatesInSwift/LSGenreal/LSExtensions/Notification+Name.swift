7783075cfa45df1bebb2293c2ff062c229e4b0e8
lihux <lihui.zl@163.com>
2017-08-27 16:38:27 +0800

    <Swift>又学到一招：通知的使用：通过扩展来实现在swift 3.0 以后的便利使用

---
+++ LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/LSGenreal/LSExtensions/Notification+Name.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Name.swift
+//  LHReadingPlan
+//
+//  Created by lihui on 2017/8/27.
+//  Copyright © 2017年 Lihux. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let readingPlanChangedNotification = Notification.Name("LH_readingPlanChangedNotification")
+}
