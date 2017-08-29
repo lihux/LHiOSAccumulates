f83224f261673de49d0ee383eb4c15360806aefd
lihux <lihui.zl@163.com>
2017-08-30 07:28:18 +0800

    <OC>csapp ch7 添加链接时打桩代码：没有编译

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter7/libraryInterpositioning/mymalloc.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter7/libraryInterpositioning/mymalloc.c
@@ -16,3 +16,24 @@ void myfree(void *ptr) {
 }
 
 #endif
+
+#ifdef LINKTIME
+
+#include <stdio.h>
+
+/*malloc wrapper function*/
+void *__real_malloc(size_t size);
+void *__real_free(void *ptr);
+
+void *__wrap_malloc(size_t size) {
+    void *ptr = __real_malloc(size);
+    printf("lihux添加的打桩方法mymalloc中调用：malloc(%d) = %p\n", (int)size, ptr);
+    return ptr;
+}
+
+void __wrap_free(void *ptr) {
+    __real_free(ptr);
+    printf("lihux添加的打桩方法myfree中调用：free (%p)\n", ptr);
+}
+
+#endif
