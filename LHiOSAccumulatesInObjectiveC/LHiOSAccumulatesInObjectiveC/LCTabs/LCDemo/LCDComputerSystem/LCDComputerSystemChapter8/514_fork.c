b8f24a7722f1b8c37314ba28908f00d9cceb19a5
lihux <lihui.zl@163.com>
2017-09-02 06:12:55 +0800

    <OC>学习fork在父、子进程返回两次的值分别所代表的意义：子进程中返回0，父进程中返回子进程的pid

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter8/514_fork.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter8/514_fork.c
@@ -4,13 +4,15 @@
 #include <stdio.h>
 /*
  结果分析：
- 关键点就在于fork()会被调用一次，但是会返回两次，一次是在父进程中，一次是在子进程中，在子进程中返回0，所以下面巧妙的通过pid == 0 来判断是在子进程中
+ 关键点就在于fork()会被调用一次，但是会返回两次，一次是在父进程中，一次是在子进程中，
+ 在父进程中返回创建的子进程的真正的PID，在子进程中返回0，所以下面巧妙的通过pid == 0
+ 来判断是在子进程中
  */
 int main() {
     pid_t pid;
     int x = 1;
     pid = fork();
-    printf("当前进程的ID为：%d   ", getpid());
+    printf("当前进程的ID为：%d   pid的值为：%d\n", getpid(), pid);
     if (pid == 0) {
         printf("child : x = %d, pid = %d, 父进程的id:%d\n", ++x, getpid(), getppid());
         exit(0);
@@ -21,6 +23,9 @@ int main() {
 
 /*
  实际运行结果：没看懂，我表示
- 当前进程的ID为：7261   parent: x = 0, pid = 7261
- 当前进程的ID为：7262   child : x = 2, pid = 7262, 父进程的id:1
- */
+ 当前进程的ID为：9920   pid的值为：9921
+ parent: x = 0, pid = 9920
+ 当前进程的ID为：9921   pid的值为：0
+ child : x = 2, pid = 9921, 父进程的id:1
+ 
+*/
