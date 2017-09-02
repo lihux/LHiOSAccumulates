832ce7cfa0ca063eb8b0f1716a5a714cd412077d
lihux <lihui.zl@163.com>
2017-09-03 06:43:04 +0800

    <OC>先创建一个进程，但是并不去kill他，导致其会一直在运行

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter8/530_kill.c
@@ -0,0 +1,14 @@
+#include "csapp.h"
+
+int main() {
+    pid_t pid;
+    if ((pid = fork()) == 0) {
+        printf("子线程ID 为：%d", getpid());
+        pause();
+        printf("这段代码绝不会被执行\n");
+        exit(0);
+    }
+    printf("子线程ID 为：%d\n", pid);
+//    kill(pid, SIGKILL);
+    exit(0);
+}
