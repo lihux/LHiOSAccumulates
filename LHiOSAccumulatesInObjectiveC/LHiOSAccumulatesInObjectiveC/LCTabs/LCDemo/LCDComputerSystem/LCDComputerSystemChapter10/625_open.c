ef723758792847b14fb9927acdab9b817f967009
lihux <lihui.zl@163.com>
2017-09-09 06:52:46 +0800

    <OC>csapp ch10 理解系统文件描述符池的概念

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter10/625_open.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter10/625_open.c
@@ -1,10 +1,12 @@
 #include "../csapp.h"
 
+//输出为3，因为0/1/2是标准输入、输出、错误的系统定义好的描述符，fd1本来使用了3，
+//但是后面它有立即关闭了，3又重新回到描述符池中，所以fd2使用的还是3！！
 int main() {
     int fd1, fd2;
-    fd1 = open("foo.txt", O_RDONLY, 0);
+    fd1 = open("foo.txt", O_RDWR, O_CREAT);
     close(fd1);
-    fd2 = open ("baz.txt", O_RDONLY, 0);
+    fd2 = open ("baz.txt", O_RDWR, O_CREAT);
     printf("fd2 = %d\n", fd2);
     exit(0);
 }
