6346c29bc255ec09aa80c9b75bf24dc1221d43cb
lihux <lihui.zl@163.com>
2017-09-10 06:40:15 +0800

    <OC>csapp ch10 通过dup2重定向描述符标注输出到文件，牛

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter10/636_foobar.c
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter10/636_foobar.c
@@ -4,9 +4,10 @@ int main() {
     int fd1, fd2;
     char c1, c2;
     fd1 = open("dog.txt", O_RDONLY, 1);
-    fd2 = open("dog.txt", O_RDONLY, 1);
+    fd2 = open("dog.txt", O_RDWR, 1);
     read(fd1, &c1, 1);
     read(fd2, &c2, 1);
+    dup2(fd2, 1);
     printf("fd1 = %d  fd2 = %d c1 = %c   c2 = %c\n", fd1, fd2, c1, c2);
     exit(0);
 }
