b0378f7012f07b2ea1cff8766d158414965c1593
lihux <lihui.zl@163.com>
2017-09-10 06:30:12 +0800

    <OC>csapp ch10 多次调用open函数会对同一个创建多个独立的文件表，以便每个都能自由的读取不同位置的文件内容

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDComputerSystem/LCDComputerSystemChapter10/636_foobar.c
@@ -0,0 +1,12 @@
+#include "../csapp.h"
+
+int main() {
+    int fd1, fd2;
+    char c1, c2;
+    fd1 = open("dog.txt", O_RDONLY, 1);
+    fd2 = open("dog.txt", O_RDONLY, 1);
+    read(fd1, &c1, 1);
+    read(fd2, &c2, 1);
+    printf("fd1 = %d  fd2 = %d c1 = %c   c2 = %c\n", fd1, fd2, c1, c2);
+    exit(0);
+}
