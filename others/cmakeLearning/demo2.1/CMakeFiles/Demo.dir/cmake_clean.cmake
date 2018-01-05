d68e28a60ad0ff2a534c6a60dceb101394614fea
李辉 <lihui.zl@163.com>
2018-01-05 15:43:51 +0800

    cmake学习：2.1 aux_source_directory(. DIR_SRCS) 获取当前文件路径下的所有源文件

---
+++ others/cmakeLearning/demo2.1/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/MathFunctions.cc.o"
+  "CMakeFiles/Demo.dir/main.cc.o"
+  "Demo.pdb"
+  "Demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
