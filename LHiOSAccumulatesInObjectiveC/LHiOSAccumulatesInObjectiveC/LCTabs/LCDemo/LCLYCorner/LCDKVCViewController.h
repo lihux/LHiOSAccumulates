7737fc56492663ea49afb7606a456e2f56e21079
李辉 <lihui.zl@163.com>
2018-08-09 10:50:08 +0800

    添加六十一道发卡弯的入口，开始新的折腾

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCLYCorner/LCDKVCViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCDKVCViewController.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by 李辉 on 2018/8/9.
+//  Copyright © 2018年 Lihux. All rights reserved.
+//
+
+#import "LCViewController.h"
+
+@interface LCDKVCViewController : LCViewController
+
+@end
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCLYCorner/LCDKVCViewController.m
@@ -0,0 +1,37 @@
+//
+//  LCDKVCViewController.m
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by 李辉 on 2018/8/9.
+//  Copyright © 2018年 Lihux. All rights reserved.
+//
+
+#import "LCDKVCViewController.h"
+
+@interface LCDKVCViewController ()
+
+@end
+
+@implementation LCDKVCViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
