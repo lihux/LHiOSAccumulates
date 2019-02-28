6eb32e7298f6d4d9cab40b5fd6b2719661ad04f2
李辉 <lihui@mucang.cn>
2019-02-28 14:28:51 +0800

    添加swift的playground玩玩LeetCode

---
+++ LHiOSAccumulatesInSwift/LHiOSAccumulatesInSwift/leetCode.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+// * Definition for singly-linked list.
+class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+    self.val = val
+    self.next = nil
+    }
+}
+
+class Solution {
+    
+    //7.整数反转：120->21, 321->123
+    func reverse(_ x: Int) -> Int {
+        var y = x > 0 ? x : -x
+        var max:Int = 0x100000000 / 2
+        max = x < 0 ? max + 1 : max
+        var result = 0
+        while y > 0 {
+            result *= 10
+            result += y % 10
+            y = y / 10
+        }
+        
+        if result > max {
+            return 0
+        }
+        return x > 0 ? result : -result
+    }
+    
+    
+    //2.两数相加
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var carrier = 0
+        var result:ListNode?, current:ListNode?
+        var p = l1, q = l2
+        while p != nil || q != nil {
+            var value = carrier + (p == nil ? 0 : p!.val) + (q == nil ? 0 : q!.val)
+            p = p?.next
+            q = q?.next
+            carrier = value / 10
+            value = value % 10
+            let node = ListNode(value)
+            
+            if result == nil {
+                result = node
+                current = node
+            } else {
+                current?.next = node
+                current = node
+            }
+        }
+        if carrier > 0 {
+            current?.next = ListNode(carrier)
+        }
+
+        return result
+    }
+    
+}
+
+let s = Solution()
+s.reverse(-2147483412)
