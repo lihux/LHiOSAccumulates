0b22d7bc4c2c6ea2ede2d313b93ae0abb39afe4b
李辉 <lihui@mucang.cn>
2019-04-29 21:38:21 +0800

    <OC>添加高考模块

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCGaoKao/LCDGKMN2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCDGKMN2ViewController.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 2019/4/29.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCSimpleTableViewController.h"
+
+@interface LCDGKMN2ViewController : LCSimpleTableViewController
+
+@end
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCGaoKao/LCDGKMN2ViewController.m
@@ -0,0 +1,16 @@
+//
+//  LCDGKMN2ViewController.m
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 2019/4/29.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCDGKMN2ViewController.h"
+
+@implementation LCDGKMN2ViewController
+
+#pragma mark - override
+
+
+@end
