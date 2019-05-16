a858332736fc26ee7f3062456d9a680bae4f73e8
李辉 <lihui.zl@163.com>
2019-05-16 13:01:50 +0800

    添加+load方法+initialize方法探索的页面

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCDBRuntime/LCDBLoadInitializeLearningViewController.h
@@ -0,0 +1,17 @@
+//
+//  LCDBLoadInitializeLearningViewController.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by 李辉 on 2019/5/16.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCDBLoadInitializeLearningViewController : LCViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCDBRuntime/LCDBLoadInitializeLearningViewController.m
@@ -0,0 +1,13 @@
+//
+//  LCDBLoadInitializeLearningViewController.m
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by 李辉 on 2019/5/16.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCDBLoadInitializeLearningViewController.h"
+
+@implementation LCDBLoadInitializeLearningViewController
+
+@end
