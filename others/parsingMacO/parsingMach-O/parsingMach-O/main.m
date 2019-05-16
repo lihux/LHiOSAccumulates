7b207ac21c6529b36466e796edc7c2f2caea2318
李辉 <lihui.zl@163.com>
2019-05-16 12:44:24 +0800

    添加分析mach-O文件的工程，尚未开始折腾

---
+++ others/parsingMacO/parsingMach-O/parsingMach-O/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  parsingMach-O
+//
+//  Created by 李辉 on 2019/5/16.
+//  Copyright © 2019 LH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
