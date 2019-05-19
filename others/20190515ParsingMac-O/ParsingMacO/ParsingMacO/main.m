4fd34efc7bfdcc14cfcde79bad9c3fd3d15f1d59
李辉 <lihui@mucang.cn>
2019-05-19 21:57:45 +0800

    testfishhook

---
+++ others/20190515ParsingMac-O/ParsingMacO/ParsingMacO/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ParsingMacO
+//
+//  Created by lihui on 2019/5/15.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <stdio.h>
+#import <stdlib.h>
+#import <mach-O/loader.h>
+#import <mach-O/swap.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
