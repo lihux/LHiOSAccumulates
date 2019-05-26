0882cf787856622db8c34db5800abe16eb04521b
李辉 <lihui@mucang.cn>
2019-05-27 07:57:37 +0800

    <OC>添加算法详情页

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCAlgorithms/commons/LCAlgorithmViewController.h
@@ -0,0 +1,17 @@
+//
+//  LCAlgorithmViewController.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 2019/5/27.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCAlgorithmViewController : LCViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCAlgorithms/commons/LCAlgorithmViewController.m
@@ -0,0 +1,32 @@
+//
+//  LCAlgorithmViewController.m
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by lihui on 2019/5/27.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCAlgorithmViewController.h"
+
+@interface LCAlgorithmViewController ()
+
+@end
+
+@implementation LCAlgorithmViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
