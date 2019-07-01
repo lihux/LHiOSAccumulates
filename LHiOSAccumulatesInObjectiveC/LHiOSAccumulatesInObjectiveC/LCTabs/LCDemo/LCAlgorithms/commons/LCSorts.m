27e70c914058838324491b9efb8bbaad0c41a70e
李辉 <lihui.zl@163.com>
2019-07-01 18:10:46 +0800

    插入排序

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCAlgorithms/commons/LCSorts.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCAlgorithms/commons/LCSorts.m
@@ -27,6 +27,7 @@ void printArray(int *nums, int n, const char *s) {
     printf("]\n");
 }
 
+/********************************************************************/
 //1. 冒泡排序，算法时间复杂度O(n^2),空间复杂度：O(1), 属于比较排序，稳定排序算法
 void bubbleSort(int *nums, int n) {
     printArray(nums, n, "\n开始冒泡排序：\n");
@@ -42,6 +43,7 @@ void bubbleSort(int *nums, int n) {
     printArray(nums, n, "\n排序之后结果是：\n");
 }
 
+/********************************************************************/
 //2. 选择排序，算法时间复杂度O(n^2),空间复杂度：O(1), 属于比较排序，稳定排序算法
 void selectSort(int *nums, int n) {
     printArray(nums, n, "\n开始选择排序：\n");
@@ -57,10 +59,28 @@ void selectSort(int *nums, int n) {
     printArray(nums, n, "\n排序之后结果是：\n");
 }
 
+/********************************************************************/
+//3. 插入排序，算法时间复杂度O(n^2),空间复杂度：O(1), 属于比较排序，稳定排序算法
+void insertSort(int *nums, int n) {
+    printArray(nums, n, "\n开始插入排序：\n");
+
+    for (int i = 1; i < n; i++) {
+        int j = i;
+        int temp = nums[i];
+        while (j > 0 && nums[j-1] > temp) {
+            nums[j] = nums[j-1];
+            j--;
+        }
+        nums[j] = temp;
+    }
+    printArray(nums, n, "\n排序之后结果是：\n");
+}
+
 void testBubbleSort() {
     int a[] = {8,9,7,6,5,4,3,2,1};
 //    bubbleSort(a, 9);
-    selectSort(a, 9);
+//    selectSort(a, 9);
+    insertSort(a, 9);
 }
 
 @implementation LCSorts
