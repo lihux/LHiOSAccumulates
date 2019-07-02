0cae2241408839916710c979fd8722eda16dc71d
李辉 <lihui@mucang.cn>
2019-07-03 06:53:11 +0800

    <OC>开发完使用CFSocket+Runloop的方式完成TCP客户端和服务端的代码请求，并将服务端直接放到了APP里

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCNetworkBasics/LCCFSocket.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCNetworkBasics/LCCFSocket.m
@@ -11,7 +11,9 @@
 #import <arpa/inet.h>
 #import <CoreFoundation/CFSocket.h>
 
-void serverCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef address, const void *data, void *info) {
+//https://www.cnblogs.com/QianChia/p/6391989.html
+
+void clientSideCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef address, const void *data, void *info) {
     if (data == NULL) {
         printf("CFSocket 建立连接成功");
     } else {
@@ -19,16 +21,73 @@ void serverCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef add
     }
 }
 
+void readStream(CFReadStreamRef iStream, CFStreamEventType eventType, void * clientCallBackInfo){
+    
+    char buf[2048];
+    bzero(buf, sizeof(buf));
+    
+    do {
+        // 接收数据
+        CFIndex dex = CFReadStreamRead(iStream, (UInt8 *)buf, sizeof(buf));
+        if (dex > 0) {
+            printf("s服务器收到数据：%s", buf);
+        }
+        
+    } while (strcmp(buf, "exit") != 0);
+}
+
+void serverSideCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef address, const void *data, void *info) {
+    if (type != kCFSocketAcceptCallBack) {
+        return;
+    }
+    CFSocketNativeHandle nativeHandle = *(CFSocketNativeHandle *)data;
+    uint8_t name[SOCK_MAXADDRLEN];
+    socklen_t nameLen = sizeof(name);
+    if (getpeername(nativeHandle, (struct sockaddr *)name, &nameLen) != 0) {
+        printf("服务器获取信息失败");
+        return;
+    }
+    struct sockaddr_in *addr_in = (struct sockaddr_in *)name;
+    char *ip_addr = inet_ntoa(addr_in->sin_addr);
+    int ip_port = addr_in->sin_port;
+    printf("\n服务器已连接：%s, port:%d\n", ip_addr, ip_port);
+
+    CFReadStreamRef iStream;
+    CFWriteStreamRef oStream;
+    
+    // 创建一组可读/写的 CFStreame
+    CFStreamCreatePairWithSocket(kCFAllocatorDefault, nativeHandle, &iStream, &oStream);
+    
+    if (iStream && oStream) {
+        
+        // 打开输入流和输出流
+        CFReadStreamOpen(iStream);
+        CFWriteStreamOpen(oStream);
+        
+        CFStreamClientContext streamContext = {0, NULL, NULL, NULL};
+        
+        // if have data to read   call the readStream function
+        if (!CFReadStreamSetClient(iStream, kCFStreamEventHasBytesAvailable, readStream, &streamContext)) {
+            exit(1);
+        }
+        
+        CFReadStreamScheduleWithRunLoop(iStream, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
+    }
+}
+
 @interface LCCFSocket ()
 
 @property (nonatomic, assign) CFSocketRef clientSocketfd;
+@property (nonatomic, assign) CFSocketRef serverSocketfd;
+@property (nonatomic, strong) dispatch_queue_t serverQueue;
 
 @end
 
 @implementation LCCFSocket
 
 - (void)connect {
-    [self connectToServer:@"127.0.0.1" port:54321];
+    [self buildServerWithIP:@"127.0.0.1" port:5431];
+    [self connectToServer:@"127.0.0.1" port:5431];
 }
 
 - (void)connectToServer:(NSString *)ip port:(int)port {
@@ -40,8 +99,7 @@ void serverCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef add
     }
     
     //建立新的连接
-    
-    self.clientSocketfd = CFSocketCreate(kCFAllocatorDefault, PF_INET, SOCK_STREAM, IPPROTO_TCP, kCFSocketConnectCallBack, serverCallBack, NULL);
+    self.clientSocketfd = CFSocketCreate(kCFAllocatorDefault, PF_INET, SOCK_STREAM, IPPROTO_TCP, kCFSocketConnectCallBack, clientSideCallBack, NULL);
     if (self.clientSocketfd) {
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
@@ -57,6 +115,37 @@ void serverCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef add
     }
 }
 
+- (void)buildServerWithIP:(NSString *)ip port:(int)port {
+    if (self.serverSocketfd) {
+        CFSocketInvalidate(self.serverSocketfd);
+        CFRelease(self.serverSocketfd);
+        self.serverSocketfd = NULL;
+        return;
+    }
+    self.serverSocketfd = CFSocketCreate(kCFAllocatorDefault, PF_INET, SOCK_STREAM, IPPROTO_TCP, kCFSocketAcceptCallBack, serverSideCallBack, NULL);
+    if (self.serverSocketfd) {
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip.UTF8String);
+        CFDataRef address = CFDataCreate(kCFAllocatorDefault, (UInt8 *)&addr, sizeof(addr));
+        int optVal = 1;
+        setsockopt(CFSocketGetNative(self.serverSocketfd), SOL_SOCKET, SO_REUSEADDR, (void *)&optVal, sizeof(optVal));
+        CFSocketError err = CFSocketSetAddress(self.serverSocketfd, address);
+        CFRelease(address);
+        if (err == kCFSocketSuccess) {
+            self.serverQueue = dispatch_queue_create("queue.server.me.lihux", DISPATCH_QUEUE_CONCURRENT);
+            dispatch_async(self.serverQueue, ^{
+                CFRunLoopRef runloop = CFRunLoopGetCurrent();
+                CFRunLoopSourceRef source = CFSocketCreateRunLoopSource(kCFAllocatorDefault, self.serverSocketfd, 0);
+                CFRunLoopAddSource(runloop, source, kCFRunLoopCommonModes);
+                CFRelease(source);
+                CFRunLoopRun();
+            });
+        }
+    }
+}
+
 - (void)sendData {
     char *str = "\nThis message was send from Lihux's Personal APP Using CFSocket!\n";
     ssize_t sendLen = send(CFSocketGetNative(self.clientSocketfd), str, strlen(str), 0);
@@ -64,6 +153,15 @@ void serverCallBack(CFSocketRef socket, CFSocketCallBackType type, CFDataRef add
         NSLog(@"发送失败");
     } else {
         NSLog(@"发送成功");
+    }
+}
+
+- (void)dealloc {
+    if (self.clientSocketfd) {
+        CFRelease(self.clientSocketfd);
+    }
+    if (self.serverSocketfd) {
+        CFRelease(self.serverSocketfd);
     }
 }
 
