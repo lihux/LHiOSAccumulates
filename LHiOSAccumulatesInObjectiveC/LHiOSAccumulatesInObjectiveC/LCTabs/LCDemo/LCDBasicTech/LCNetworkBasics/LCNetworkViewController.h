b57812d02be35f83fbb6979486fe3d20b8deb203
李辉 <lihui.zl@163.com>
2019-07-02 19:06:31 +0800

    添加测试网络库的代码

---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCNetworkBasics/LCNetworkViewController.h
@@ -0,0 +1,17 @@
+//
+//  LCNetworkViewController.h
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by 李辉 on 2019/7/2.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCNetworkViewController : LCViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCNetworkBasics/LCNetworkViewController.m
@@ -0,0 +1,31 @@
+//
+//  LCNetworkViewController.m
+//  LHiOSAccumulatesInObjectiveC
+//
+//  Created by 李辉 on 2019/7/2.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+#import "LCNetworkViewController.h"
+
+@interface LCNetworkViewController ()
+
+@end
+
+@implementation LCNetworkViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+- (IBAction)didTapOnBSDSocketButton:(id)sender {
+}
+- (IBAction)didTapOnCFSocketButton:(id)sender {
+}
+
+- (IBAction)didTapOnCFNetworkButton:(id)sender {
+}
+- (IBAction)didTapOnNSURLConnectionButton:(id)sender {
+}
+- (IBAction)didTapOnNSURLSessionButton:(id)sender {
+}
+@end
