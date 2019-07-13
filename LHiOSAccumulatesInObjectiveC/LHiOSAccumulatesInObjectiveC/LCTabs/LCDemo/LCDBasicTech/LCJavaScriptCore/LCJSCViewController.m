392b74389ed7bd4296f1ae6ea9795278e37516bc
李辉 <lihui@mucang.cn>
2019-07-13 08:48:43 +0800

    <OC>继续复习JSC：如何调用OC方法

--- LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCJavaScriptCore/LCJSCViewController.m
+++ LHiOSAccumulatesInObjectiveC/LHiOSAccumulatesInObjectiveC/LCTabs/LCDemo/LCDBasicTech/LCJavaScriptCore/LCJSCViewController.m
@@ -10,6 +10,17 @@
 
 #import <JavaScriptCore/JavaScriptCore.h>
 
+@interface LCJSObject : NSObject <JSExport>
+@end
+
+@implementation LCJSObject
+
+- (NSInteger) add:(NSInteger)a to:(NSInteger)b {
+    return a + b;
+}
+
+@end
+
 @interface LCJSCViewController ()
 
 @property (nonatomic, strong) JSContext *context;
@@ -24,9 +35,11 @@
 }
 
 - (void)test {
-    JSValue *value = [self.context evaluateScript:@"f(2, 3)"];
-    JSValue *value2 = [self.context evaluateScript:@"add(2, 3)"];
-    NSLog(@"哈哈哈哈哈哈哈哈哈哈：%@, %@", [value toString], value2);
+    [self.context evaluateScript:@"f(2, 3)"];//f没有定义，所以抛出异常
+    JSValue *addValue = [self.context evaluateScript:@"add(2, 3)"];//js执行的是oc添加的js函数
+    JSValue *subValue = [self.context evaluateScript:@"sub(4, 1)"];//js执行的是oc 添加的oc函数
+    NSString *str = [NSString stringWithFormat:@"哈哈哈哈哈哈哈哈哈哈：加法：%@, 减法：%@", [addValue toString], subValue];
+    [self log:str];
 }
 
 #pragma mark - lazy load
@@ -37,6 +50,9 @@
     _context = [[JSContext alloc] init];
     [_context evaluateScript:@"function add(x, y) {return x + y}"];
 //    _context[@"add"] = @"function add(x, y) { return x + y;}";
+    _context[@"sub"] = ^(NSInteger a, NSInteger b) {
+        return a - b;
+    };
     _context.exceptionHandler = ^(JSContext *context, JSValue *value) {
         NSLog(@"JS执行发生了异常，快来看看有什么问题：%@, %@", context, value);
     };
